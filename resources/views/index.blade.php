3775fdf9516b5a78fd50b42c0356c7d6728623eb
Ricardo Lima <zcincoss@gmail.com>
2023-09-21 00:26:49 -0300

    fix: separed test, add events test, removed comment

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,9 +18,7 @@
     <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/index.global.min.js'></script>
     <script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>
 
-    <script>
-       
-    </script>
+    <script></script>
 
     <script src="js/sweetalert2.all.min.js"></script>
     <link href="js/fullcalendar/lib/main.css" rel="stylesheet" />
@@ -39,7 +37,7 @@
                         @auth
                             <form method="POST" action="{{ route('logout') }}">
                                 @csrf
-                                <button type="submit" class="btn btn-primary">Logout</button>
+                                <button type="submit" class="btn btn-primary">Sair</button>
                             </form>
                         @else
                             <a href="{{ route('login') }}" class="btn btn-info">Login</a>
@@ -47,16 +45,14 @@
                         <a onclick="NewEvento()" class="btn btn-success" data-toggle="modal">
                             <i class="material-icons">&#xE147;</i> <span>Criar novo evento</span>
                         </a>
-                        <a href="#deleteEmployeeModal" class="btn btn-danger" data-toggle="modal">
-                            <i class="material-icons">&#xE15C;</i> <span>Remover</span>
-                        </a>
+                        
                     </div>
                     <!-- <div class="col-sm-6">
                     @auth
-                            <form method="POST" action="{{ route('logout') }}">
-                                @csrf
-                                <button type="submit" class="btn btn-primary">Logout</button>
-                            </form>
+                                <form method="POST" action="{{ route('logout') }}">
+                                    @csrf
+                                    <button type="submit" class="btn btn-primary">Logout</button>
+                                </form>
 @else
     <a href="{{ route('login') }}" class="btn btn-primary">Login</a>
                     @endauth
@@ -72,12 +68,7 @@
             <table class="table table-striped table-hover">
                 <thead>
                     <tr>
-                        <th>
-                            <span class="custom-checkbox">
-                                <input type="checkbox" id="selectAll">
-                                <label for="selectAll"></label>
-                            </span>
-                        </th>
+                    
                         <th>Titulo</th>
                         <th>Sobre</th>
                         <th>Status</th>
@@ -101,14 +92,23 @@
         </div>
     </div>
     <div class="container" id='calendar'></div>
-    
+
 </body>
 
 </html>
 
 <script>
     // Função que faz consumo e distribuição dos eventos
-
+    function formatarDT(dataStr) {
+    const data = new Date(dataStr);
+    const dia = String(data.getDate()).padStart(2, '0');
+    const mes = String(data.getMonth() + 1).padStart(2, '0'); // Lembre-se de que os meses são base 0 em JavaScript
+    const ano = data.getFullYear();
+    const horas = String(data.getHours()).padStart(2, '0');
+    const minutos = String(data.getMinutes()).padStart(2, '0');
+    
+    return `${dia}/${mes}/${ano} ${horas}:${minutos}`;
+}
     $.ajax({
         type: 'GET',
         url: '/eventos',
@@ -118,14 +118,11 @@
             var tableBody = $('#eventosTable');
             $.each(eventos, function(index, evento) {
                 var row = '<tr>' +
-                    '<td><span class="custom-checkbox"><input type="checkbox" id="checkbox' + evento
-                    .id + '" name="options[]" value="' + evento.id + '"><label for="checkbox' +
-                    evento.id + '"></label></span></td>' +
                     '<td>' + evento.title + '</td>' +
                     '<td>' + evento.description + '</td>' +
                     '<td>' + evento.status + '</td>' +
-                    '<td>' + evento.start + '</td>' +
-                    '<td>' + evento.end + '</td>' +
+                    '<td>' + formatarDT(evento.start) + '</td>' +
+                    '<td>' + formatarDT(evento.end) + '</td>' +
                     '<td>' + evento.usr_responsavel + '</td>' +
 
                     '<td>' +
@@ -183,7 +180,7 @@
         });
     }
 
-    function NewEvento(x) {
+    function NewEvento() {
         Swal.fire({
             title: 'Adicionar Evento',
             html: '<div class="custom-swal-modal" style="display: flex; flex-direction: column; align-items: center; justify-content: center; padding: 20px">' +
@@ -274,162 +271,113 @@
 
 
     function EditarEvento(x) {
-        var title = $(x).data('title'); // Obtém o título do atributo de dados
-        var url = 'eventos/' + encodeURIComponent(title);
+    var title = $(x).data('title'); // Obtém o título do atributo de dados
+    var url = '/eventos/' + encodeURIComponent(title); // Correção na construção da URL
+    var url2 = '/editar-evento/' + encodeURIComponent(title); // Correção na construção da URL
 
-        $.ajax({
-            type: 'GET',
-            url: url,
-            success: function(data) {
-                console.log(data);
-                var eventos = data.eventos;
-                var dataInicioFormatada = formatarData(eventos.start);
-                var dataPrazoFormatada = formatarData(eventos.end);
+    $.ajax({
+        type: 'GET',
+        url: url,
+        success: function(data) {
+            console.log(data);
+            var eventos = data.eventos;
+            var dataInicioFormatada = formatarDT(eventos.start);
+            var dataPrazoFormatada = formatarDT(eventos.end);
+            console.log(dataPrazoFormatada);
+            Swal.fire({
+                title: 'Editar Evento',
+                html: '<div class="custom-swal-modal" style="display: flex; flex-direction: column; align-items: center; justify-content: center; padding: 20px">' +
+                    '<input id="swalEvtTitle" class="swal2-input custom-swal-input" style="width: 100%;" placeholder="Digite o título" value="' + eventos.title + '">' +
+                    '<input id="swalEvtDesc" class="swal2-input custom-swal-input" style="width: 100%; margin-top: 10px" placeholder="Digite a descrição" value="' + eventos.description + '"></input>' +
+                    '<div class="swal2-radio-group">' +
+                    '<p class="mt-2">Status</p>' +
+                    '<label class="swal2-radio">' +
+                    '<input type="radio" id="swalEvtStatusAberto" class="swal2-radio-input" name="swalEvtStatus" value="Aberto" ' + (eventos.status === 'Aberto' ? 'checked' : '') + '>' +
+                    '<span class="swal2-radio-label">Aberto</span>' +
+                    '</label>' +
+                    '<label class="swal2-radio">' +
+                    '<input type="radio" id="swalEvtStatusConcluido" class="swal2-radio-input" name="swalEvtStatus" value="Concluído" ' + (eventos.status === 'Concluído' ? 'checked' : '') + '>' +
+                    '<span class="swal2-radio-label">Concluído</span>' +
+                    '</label>' +
+                    '</div>' +
+                    '<label for="swalEvtStartDate" class="custom-swal-title" style="width: 100%">Data Inicial:</label>' +
+                    '<input type="datetime-local" id="swalEvtStartDate" class="swal2-input" style="width: 100%;" value="' + dataInicioFormatada + '">' +
+                    '<label for="swalEvtEndDate" class="custom-swal-title">Data Final:</label>' +
+                    '<input type="datetime-local" id="swalEvtEndDate" class="swal2-input" style="width: 100%;" value="' + dataPrazoFormatada + '">' +
+                    '</div>',
+                showCancelButton: true,
+                confirmButtonText: 'Editar',
+                cancelButtonText: 'Cancelar',
+                customClass: {
+                    popup: 'custom-swal-modal',
+                    title: 'custom-swal-title',
+                    confirmButton: 'custom-swal-confirm-button',
+                    cancelButton: 'custom-swal-cancel-button',
+                },
+                preConfirm: function() {
+                    var eventTitle = document.getElementById('swalEvtTitle').value;
+                    var eventDescription = document.getElementById('swalEvtDesc').value;
+                    var startDate = new Date(document.getElementById('swalEvtStartDate').value);
+                    var endDate = new Date(document.getElementById('swalEvtEndDate').value);
 
-                var pai = document.createElement('div');
-                pai.innerHTML = `<div class="modal-dialog">
-                <div class="modal-content">
-                    <form id="editEventoForm"> <!-- Adicione um ID ao formulário -->
-                        @csrf <!-- Adicione o token CSRF para proteção contra ataques CSRF -->
+                    // Formatar as datas no formato 'Y-m-d H:i:s'
+                    var formattedStartDate = startDate.toISOString().slice(0, 19).replace("T", " ");
+                    var formattedEndDate = endDate.toISOString().slice(0, 19).replace("T", " ");
 
-                        <div class="modal-header">
-                            <h4 class="modal-title">Editar Evento ` + eventos.title + `</h4>
-                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                        </div>
-                        <div class="modal-body">
-                            <div class="form-group">
-                                <label>Título</label>
-                                <input type="text" name="title" value='` + eventos.title + `' class="form-control" required>
-                            </div>
-                            <div class="form-group">
-                                <label>Descrição</label>
-                                <input type="text" name="descricao" value='` + eventos.descricao + `' class="form-control" required>
-                            </div>
-                            <div class="form-group">
-                                <label>Status</label>
-                                <select name="status" class="form-control value='` + eventos.status + `'">
-                                    <option value=""></option>
-                                    <option value="Concluido">Concluído</option>
-                                    <option value="Andamento">Em Andamento</option>
-                                </select>
-                            </div>
-                            <div class="form-group">
-                                <label>Data de Início</label>
-                                <input type="date" value="` + dataInicioFormatada + `" name="data_inicio" class="form-control" required>
-                            </div>
-                            <div class="form-group">
-                                <label>Data de Encerramento</label>
-                                <input type="date" name="data_prazo" value="` + dataPrazoFormatada + `" class="form-control" required>
-                            </div>
-                        </div>
-                        <div class="modal-footer">
-                            <input type="button" class="btn btn-default" data-dismiss="modal" value="Cancelar">
-                            <input type="submit" class="btn btn-success" value="Adicionar">
-                        </div>
-                    </form>
-                </div>
-            </div>
-        `;
+                    console.log(formattedStartDate);
 
-                // Encontre a div com o ID editEmployeeModal
-                var editModal = document.getElementById('editEmployeeModal');
+                    if (!eventTitle) {
+                        Swal.showValidationMessage('O título do evento é obrigatório.');
+                        return false;
+                    }
 
-                // Limpe qualquer conteúdo existente na div
-                editModal.innerHTML = '';
+                    if (startDate > endDate) {
+                        Swal.showValidationMessage('A data final não pode ser anterior à data inicial.');
+                        return false;
+                    }
 
-                // Adicione o formulário à div
-                editModal.appendChild(pai);
-                $('#editEventoForm').submit(function(e) {
-                    e.preventDefault(); // Impede o envio padrão do formulário
+                    var eventData = {
+                        title: eventTitle,
+                        description: eventDescription,
+                        status: $("input[name='swalEvtStatus']:checked").val(), // Obter o valor do input de rádio selecionado
+                        start: formattedStartDate,
+                        end: formattedEndDate
+                    };
 
-                    var formData = $(this).serialize(); // Serialize os dados do formulário
-                    var url = 'editar-evento/' + encodeURIComponent(title);
-
+                    // Sending AJAX request
                     $.ajax({
                         type: 'put',
-                        url: url, // Substitua pelo URL correto da sua rota de criação de evento
-                        data: formData,
+                        url: url2, // Substitua pelo URL correto da sua rota de edição de evento
+                        data: eventData, // Envie os dados do evento editado
                         success: function(response) {
                             // Manipule a resposta da API, se necessário
                             console.log(response);
-                            // Feche o modal
-                            $('#editEmployeeModal').modal('hide');
+                            Swal.fire('Evento editado com sucesso!', '', 'success').then(function() {
+                                location.reload(); // Recarrega a página após clicar em "OK"
+                            });
                         },
                         error: function(error) {
                             console.error('Erro na solicitação AJAX:', error);
                         }
                     });
-                });
-            },
-            error: function(error) {
-                console.error('Erro na solicitação AJAX:', error);
-            }
-        });
 
-
-    }
-
-    $('#addEventoForm').submit(function(e) {
-        e.preventDefault(); // Impede o envio padrão do formulário
-
-        var formData = $(this).serialize(); // Serialize os dados do formulário
-
-        $.ajax({
-            type: 'POST',
-            url: 'criar-evento', // Substitua pelo URL correto da sua rota de criação de evento
-            data: formData,
-            success: function(response) {
-                // Manipule a resposta da API, se necessário
-                console.log(response);
-                // Feche o modal
-                $('#addEmployeeModal').modal('hide');
-            },
-            error: function(error) {
-                console.error('Erro na solicitação AJAX:', error);
-            }
-        });
-    });
-
-
-    function formatarData(dataHoraStr) {
-        var partes = dataHoraStr.split(' ')[0].split('-');
-        if (partes.length === 3) {
-            var ano = partes[0];
-            var mes = partes[1];
-            var dia = partes[2];
-            return ano + '-' + mes + '-' + dia;
-        }
-        // Retorna a data original se não for possível formatar
-        return dataHoraStr;
-    }
-
-    $('.delete').click(function(e) {
-        e.preventDefault(); // Impede o comportamento padrão do link
-
-        var title = $(this).data('title'); // Obtém o título do atributo de dados
-
-        if (confirm('Tem certeza que deseja excluir o evento "' + title + '"?')) {
-            var url = '/excluir-evento/' +
-            title; // Substitua pelo URL correto da sua rota de exclusão de evento
-
-            $.ajax({
-                type: 'DELETE', // Use o método DELETE para solicitar exclusão
-                url: url,
-                success: function(response) {
-                    // Manipule a resposta da API, se necessário
-                    console.log(response);
-                    // Recarregue a página ou atualize a lista de eventos, se necessário
-                },
-                error: function(error) {
-                    console.error('Erro na solicitação AJAX:', error);
+                    return false;
+                }
+            }).then(function(result) {
+                if (result.isConfirmed) {
+                    Swal.fire('Evento editado com sucesso!', '', 'success');
                 }
             });
+        },
+        error: function(error) {
+            console.error('Erro na solicitação AJAX:', error);
         }
     });
+}
+
+
+
+
+
+
 </script>
-
-
-
-
-
-
--- tests/Feature/EventoTest.php
+++ tests/Feature/EventoTest.php
@@ -4,40 +4,16 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\Models\EventoModel;
 
-class ExampleTest extends TestCase
+
+class EventoTest extends TestCase
 {
     /**
-     * A basic test example.
+     *
      *
      * @return void
      */
-    public function test_user_registration()
-    {
-        $response = $this->post('http://localhost:8000/register', [
-            'name' => 'Johnss Doe',
-            'email' => 'john@example.com',
-            'password' => 'password123',
-            'password_confirmation' => 'password123',
-        ]);
-    
-        $response->assertStatus(302);
-    }
-
-    public function login(Request $request)
-    {
-        $credentials = $request->only('email', 'password');
-    
-        if (Auth::attempt($credentials)) {
-            $user = Auth::user();
-            $token = $user->createToken('token-name')->plainTextToken;
-    
-            return response()->json(['token' => $token], 200);
-        }
-    
-        return response()->json(['message' => 'Invalid credentials'], 401);
-    }
-
     public function test_user_login()
     {
         $response = $this->post('http://localhost:8000/login', [
@@ -47,10 +23,8 @@ class ExampleTest extends TestCase
     
         $response->assertStatus(302);
     
-        // Check for the 'laravel_session' cookie
         $response->assertCookie('laravel_session');
     
-        // Extract the cookie value (token)
         $token = $response->cookie('laravel_session');
     
         return $token;
@@ -60,7 +34,6 @@ class ExampleTest extends TestCase
     {
         $token = $this->test_user_login();
 
-        // Manually create an event
         $response = $this->json('POST', 'http://localhost:8000/criar-evento', [
             'title' => 'Exemplo de Título',
             'description' => 'Exemplo de Descrição',
@@ -68,31 +41,25 @@ class ExampleTest extends TestCase
             'end' => '2023-09-30',
             'usr_responsavel' => 'john@example.com',
         ], ['Authorization' => "Bearer $token"]);
-    
+
         $response->assertStatus(302);
+        
+        $event = EventoModel::where([
+            'title' => 'Exemplo de Título',
+            'description' => 'Exemplo de Descrição',
+            'start' => '2023-09-20',
+            'end' => '2023-09-30',
+            'usr_responsavel' => 'john@example.com',
+        ])->first();
+
+        $this->assertNotNull($event);
     }
 
-<<<<<<< HEAD
-    public function test_edit_event()
-=======
-        public function test_get_event()
-    {
-        $event = factory()->create(); // Suponha que você tenha um evento criado
-
-        $response = $this->get("http://localhost:8000/visualizar-evento/{$event->id}");
-
-        $response->assertStatus(200) // Deve retornar código 200 (OK)
-            ->assertSee($event->title) // Verifica se o título do evento está presente na resposta
-            ->assertSee($event->description); // Verifica se a descrição do evento está presente na resposta
-    }
-
-        public function test_edit_event()
->>>>>>> 38aef28765817a91db042096e268f529579469b1
+    public function test_update_event()
     {
         $token = $this->test_user_login();
 
-        // Manually create an event
-        $event = Event::create([
+        $event = EventoModel::create([
             'title' => 'Evento para Edição',
             'description' => 'Descrição do Evento',
             'start' => '2023-09-20',
@@ -107,27 +74,36 @@ class ExampleTest extends TestCase
             'end' => '2023-11-10',
             'usr_responsavel' => 'john@example.com',
         ];
-    
+
         $response = $this->put("http://localhost:8000/editar-evento/{$event->id}", $data, ['Authorization' => "Bearer $token"]);
-    
+
         $response->assertStatus(302);
+
+        $event->refresh();
+
+        $this->assertEquals($data['title'], $event->title);
+        $this->assertEquals($data['description'], $event->description);
+        $this->assertEquals($data['start'], $event->start->format('Y-m-d'));
+        $this->assertEquals($data['end'], $event->end->format('Y-m-d'));
+        $this->assertEquals($data['usr_responsavel'], $event->usr_responsavel);
     }
 
     public function test_delete_event()
     {
         $token = $this->test_user_login();
 
-        // Manually create an event
-        $event = Event::create([
+        $event = EventoModel::create([
             'title' => 'Evento para Exclusão',
             'description' => 'Descrição do Evento',
             'start' => '2023-09-20',
             'end' => '2023-09-30',
             'usr_responsavel' => 'john@example.com',
         ]);
-    
+
         $response = $this->delete("http://localhost:8000/excluir-evento/{$event->id}", [], ['Authorization' => "Bearer $token"]);
-    
+
         $response->assertStatus(302);
+
+        $this->assertDatabaseMissing('events', ['id' => $event->id]);
     }
 }
